dbbaf44afd8080a14ef5770fddf90001328862fc
LeMinHieu <lminhhieu2507@gmail.com>
2025-12-14 22:57:51 +0700

    chore(init): add initial Spring Boot application for vnrail-api with Maven configuration

---
+++ backend/application/vnrail-api/src/main/java/org/vnrail/api/VnrailApplication.java
@@ -0,0 +1,13 @@
+package org.vnrail.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnrailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VnrailApplication.class, args);
+    }
+
+}
---
+++ backend/application/vnrail-api/src/test/java/org/vnrail/api/VnrailApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vnrail.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnrailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
