b79a4e3c61af40af5eb1031d3799e3defff0bd50
LeMinHieu <lminhhieu2507@gmail.com>
2025-12-15 18:37:37 +0700

    chore(init): add vnrail-common module with Maven Wrapper, Spring Boot configuration, and initial project structure

---
+++ backend/core/vnrail-common/src/main/java/org/vnrail/vnrailcommon/VnrailCommonApplication.java
@@ -0,0 +1,13 @@
+package org.vnrail.vnrailcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnrailCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VnrailCommonApplication.class, args);
+    }
+
+}
---
+++ backend/core/vnrail-common/src/test/java/org/vnrail/vnrailcommon/VnrailCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vnrail.vnrailcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnrailCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
