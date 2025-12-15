494e7a6b924ca02d6f4e8b2de99bebaecfb29b27
LeMinHieu <lminhhieu2507@gmail.com>
2025-12-15 18:41:51 +0700

    chore(init): add vnrail-security module with Maven Wrapper, Spring Boot configuration, and initial project structure

---
+++ backend/core/vnrail-security/src/main/java/org/vnrail/vnrailsecurity/VnrailSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.vnrail.vnrailsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnrailSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VnrailSecurityApplication.class, args);
+    }
+
+}
---
+++ backend/core/vnrail-security/src/test/java/org/vnrail/vnrailsecurity/VnrailSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vnrail.vnrailsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnrailSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
